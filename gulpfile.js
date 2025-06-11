d7ddb1338710976604c36a0511e22a67dd1ba26f
LucGabriel08 <seu-email@github.com>
2025-06-10 22:02:02 -0300

    criando o hero e importando a font avenir

--- gulpfile.js
+++ gulpfile.js
@@ -3,8 +3,9 @@ const sass = require('gulp-sass')(require('sass'));
 const imagemin = require('gulp-imagemin');
 
 function styles() {
-    return gulp.src('./src/styles/*.scss')
+    return gulp.src('./src/styles/main.scss')
     .pipe(sass({outputStyle: 'compressed'}))
+    .on('error', sass.logError) 
     .pipe(gulp.dest('./dist/css'));
 }
 
